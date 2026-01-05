862bf97b28f32d136052eff7334eca04d6be3a60
Yaroslav Biletskiy <erz9engel@googlemail.com>
2026-01-05 12:56:44 +0100

    Ported DomainTests

--- tests/unit/test_integration_mirror.py
+++ tests/unit/test_integration_mirror.py
@@ -51,3 +51,286 @@ class MessagesTests(unittest.TestCase):
         m_post.return_value = mock_response(400, {"message": "Invalid from"})
         req = self.client.messages.create(data={"from": "sdsdsd"}, domain=self.domain)
         self.assertEqual(req.status_code, 400)
+
+
+class DomainTests(unittest.TestCase):
+    """Mirror of integration DomainTests with mocked HTTP."""
+
+    def setUp(self) -> None:
+        self.client = Client(auth=AUTH)
+        self.domain = DOMAIN
+        self.test_domain = TEST_DOMAIN
+        self.post_domain_data = {"name": self.test_domain}
+        self.put_domain_data = {"spam_action": "disabled"}
+        self.post_domain_creds = {
+            "login": f"alice_bob@{self.domain}",
+            "password": "test_new_creds123",
+        }
+        self.put_domain_creds = {"password": "test_new_creds"}
+        self.put_domain_connections_data = {"require_tls": "false", "skip_verification": "false"}
+        self.put_domain_tracking_data = {"active": "yes", "skip_verification": "false"}
+        self.put_domain_unsubscribe_data = {
+            "active": "yes",
+            "html_footer": "\n<br>\n<p><a href=\"%unsubscribe_url%\">UnSuBsCrIbE</a></p>\n",
+            "text_footer": "\n\nTo unsubscribe here click: <%unsubscribe_url%>\n\n",
+        }
+        self.put_domain_dkim_authority_data = {"self": "false"}
+        self.put_domain_webprefix_data = {"web_prefix": "python"}
+        self.put_dkim_selector_data = {"dkim_selector": "s"}
+
+    @patch("mailgun.client.requests.delete")
+    @patch("mailgun.client.requests.post")
+    def test_post_domain(self, m_post: MagicMock, m_delete: MagicMock) -> None:
+        m_delete.return_value = mock_response(200, {"message": "ok"})
+        m_post.return_value = mock_response(
+            200, {"message": "Domain DNS records have been created"}
+        )
+        request = self.client.domains.create(data=self.post_domain_data)
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("Domain DNS records have been created", request.json()["message"])
+
+    @patch("mailgun.client.requests.post")
+    def test_post_domain_creds(self, m_post: MagicMock) -> None:
+        m_post.return_value = mock_response(200, {"message": "Created"})
+        request = self.client.domains_credentials.create(
+            domain=self.domain, data=self.post_domain_creds
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.delete")
+    @patch("mailgun.client.requests.post")
+    @patch("mailgun.client.requests.put")
+    def test_update_simple_domain(
+        self, m_put: MagicMock, m_post: MagicMock, m_delete: MagicMock
+    ) -> None:
+        m_delete.return_value = mock_response(200)
+        m_post.return_value = mock_response(200, {"domain": {}})
+        m_put.return_value = mock_response(200, {"message": "Domain has been updated"})
+        self.client.domains.create(data=self.post_domain_data)
+        request = self.client.domains.put(
+            data={"spam_action": "disabled"}, domain=self.post_domain_data["name"]
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertEqual(request.json()["message"], "Domain has been updated")
+
+    @patch("mailgun.client.requests.post")
+    @patch("mailgun.client.requests.put")
+    def test_put_domain_creds(self, m_put: MagicMock, m_post: MagicMock) -> None:
+        m_post.return_value = mock_response(200, {"message": "Created"})
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        self.client.domains_credentials.create(
+            domain=self.domain, data=self.post_domain_creds
+        )
+        request = self.client.domains_credentials.put(
+            domain=self.domain, data=self.put_domain_creds, login="alice_bob"
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.post")
+    @patch("mailgun.client.requests.put")
+    def test_put_mailboxes_credentials(self, m_put: MagicMock, m_post: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_put.return_value = mock_response(
+            200,
+            {
+                "message": "Password changed",
+                "note": "",
+                "credentials": {f"alice_bob@{self.domain}": {}},
+            },
+        )
+        self.client.domains_credentials.create(
+            domain=self.domain, data=self.post_domain_creds
+        )
+        req = self.client.mailboxes.put(
+            domain=self.domain, login=f"alice_bob@{self.domain}"
+        )
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("Password changed", req.json()["message"])
+
+    @patch("mailgun.client.requests.get")
+    def test_get_domain_list(self, m_get: MagicMock) -> None:
+        m_get.return_value = mock_response(200, {"items": []})
+        req = self.client.domainlist.get()
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("items", req.json())
+
+    @patch("mailgun.client.requests.get")
+    def test_get_smtp_creds(self, m_get: MagicMock) -> None:
+        m_get.return_value = mock_response(200, {"items": []})
+        request = self.client.domains_credentials.get(domain=self.domain)
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("items", request.json())
+
+    @patch("mailgun.client.requests.get")
+    def test_get_sending_queues(self, m_get: MagicMock) -> None:
+        m_get.return_value = mock_response(200, {"scheduled": [], "retry": []})
+        request = self.client.domains_sendingqueues.get(domain=self.test_domain)
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("scheduled", request.json())
+
+    @patch("mailgun.client.requests.get")
+    @patch("mailgun.client.requests.post")
+    def test_get_single_domain(self, m_post: MagicMock, m_get: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_get.return_value = mock_response(200, {"domain": {"name": self.test_domain}})
+        self.client.domains.create(data=self.post_domain_data)
+        req = self.client.domains.get(domain_name=self.post_domain_data["name"])
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("domain", req.json())
+
+    @patch("mailgun.client.requests.put")
+    @patch("mailgun.client.requests.post")
+    def test_verify_domain(self, m_post: MagicMock, m_put: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_put.return_value = mock_response(200, {"domain": {"state": "verified"}})
+        self.client.domains.create(data=self.post_domain_data)
+        req = self.client.domains.put(domain=self.post_domain_data["name"], verify=True)
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("domain", req.json())
+
+    @patch("mailgun.client.requests.put")
+    def test_put_domain_connections(self, m_put: MagicMock) -> None:
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        request = self.client.domains_connection.put(
+            domain=self.domain, data=self.put_domain_connections_data
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    def test_put_domain_tracking_open(self, m_put: MagicMock) -> None:
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        request = self.client.domains_tracking_open.put(
+            domain=self.domain, data=self.put_domain_tracking_data
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    def test_put_domain_tracking_click(self, m_put: MagicMock) -> None:
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        request = self.client.domains_tracking_click.put(
+            domain=self.domain, data=self.put_domain_tracking_data
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    def test_put_domain_unsubscribe(self, m_put: MagicMock) -> None:
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        request = self.client.domains_tracking_unsubscribe.put(
+            domain=self.domain, data=self.put_domain_unsubscribe_data
+        )
+        self.assertEqual(request.status_code, 200)
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    @patch("mailgun.client.requests.post")
+    def test_put_dkim_authority(self, m_post: MagicMock, m_put: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        self.client.domains.create(data=self.post_domain_data)
+        request = self.client.domains_dkimauthority.put(
+            domain=self.test_domain, data=self.put_domain_dkim_authority_data
+        )
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    @patch("mailgun.client.requests.post")
+    def test_put_webprefix(self, m_post: MagicMock, m_put: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        self.client.domains.create(data=self.post_domain_data)
+        request = self.client.domains_webprefix.put(
+            domain=self.test_domain, data=self.put_domain_webprefix_data
+        )
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.put")
+    def test_put_dkim_selector(self, m_put: MagicMock) -> None:
+        m_put.return_value = mock_response(200, {"message": "Updated"})
+        request = self.client.domains_dkimselector.put(
+            domain=self.domain, data=self.put_dkim_selector_data
+        )
+        self.assertIn("message", request.json())
+
+    @patch("mailgun.client.requests.get")
+    def test_get_dkim_keys(self, m_get: MagicMock) -> None:
+        m_get.return_value = mock_response(
+            200,
+            {
+                "items": [
+                    {
+                        "signing_domain": "python.test.domain5",
+                        "selector": "smtp",
+                        "dns_record": {},
+                    }
+                ],
+                "paging": {},
+            },
+        )
+        data = {
+            "page": "string",
+            "limit": "0",
+            "signing_domain": "python.test.domain5",
+            "selector": "smtp",
+        }
+        req = self.client.dkim_keys.get(data=data)
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("items", req.json())
+        self.assertIn("paging", req.json())
+
+    @patch("mailgun.client.requests.delete")
+    def test_delete_dkim_keys(self, m_delete: MagicMock) -> None:
+        m_delete.return_value = mock_response(200, {"message": "success"})
+        query = {"signing_domain": "python.test.domain5", "selector": "smtp"}
+        req = self.client.dkim_keys.delete(filters=query)
+        self.assertEqual(req.status_code, 200)
+        self.assertIn("success", req.json()["message"])
+
+    @patch("mailgun.client.requests.delete")
+    @patch("mailgun.client.requests.post")
+    def test_delete_domain_creds(self, m_post: MagicMock, m_delete: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_delete.return_value = mock_response(200)
+        self.client.domains_credentials.create(
+            domain=self.domain, data=self.post_domain_creds
+        )
+        request = self.client.domains_credentials.delete(
+            domain=self.domain, login="alice_bob"
+        )
+        self.assertEqual(request.status_code, 200)
+
+    @patch("mailgun.client.requests.delete")
+    @patch("mailgun.client.requests.post")
+    def test_delete_all_domain_credentials(
+        self, m_post: MagicMock, m_delete: MagicMock
+    ) -> None:
+        m_post.return_value = mock_response(200)
+        m_delete.return_value = mock_response(
+            200, {"message": "All domain credentials have been deleted"}
+        )
+        self.client.domains_credentials.create(
+            domain=self.domain, data=self.post_domain_creds
+        )
+        request = self.client.domains_credentials.delete(domain=self.domain)
+        self.assertEqual(request.status_code, 200)
+        self.assertIn(
+            request.json()["message"], "All domain credentials have been deleted"
+        )
+
+    @patch("mailgun.client.requests.delete")
+    @patch("mailgun.client.requests.post")
+    def test_delete_domain(self, m_post: MagicMock, m_delete: MagicMock) -> None:
+        m_post.return_value = mock_response(200)
+        m_delete.return_value = mock_response(
+            200, {"message": "Domain will be deleted in the background"}
+        )
+        self.client.domains.create(data=self.post_domain_data)
+        request = self.client.domains.delete(domain=self.test_domain)
+        self.assertEqual(
+            request.json()["message"], "Domain will be deleted in the background"
+        )
+        self.assertEqual(request.status_code, 200)
